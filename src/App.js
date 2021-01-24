4bdb04894ef2e13cc82850a52cec5cf8a8f35b04
Vistrit <vistritpandey@gmail.com>
2021-01-24 17:39:13 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
+import Chat from "./Chat";
+import Login from "./Login";
+import Sidebar from "./Sidebar";
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  return <div className="App"></div>;
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <BrowserRouter>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <div className="app__body-emptyChat">
+                </div>
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
