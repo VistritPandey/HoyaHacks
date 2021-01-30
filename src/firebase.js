e320bef96aaad2804860e66bf1b70213a4332a29
vistritpandey <vistritpandey@gmail.com>
2021-01-30 16:02:01 +0530

    k

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,19 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA2pi0RiZF3VeU2PIRx1VG9qu8J0J9TRjg",
-    authDomain: "cred-news.firebaseapp.com",
-    projectId: "cred-news",
-    storageBucket: "cred-news.appspot.com",
-    messagingSenderId: "220168688339",
-    appId: "1:220168688339:web:fbdeaa4d704d3ed34f7a26",
-    measurementId: "G-HRLWYS50C8"
-  };
+  apiKey: "AIzaSyA2pi0RiZF3VeU2PIRx1VG9qu8J0J9TRjg",
+  authDomain: "cred-news-admin.firebaseapp.com",
+  projectId: "cred-news",
+  storageBucket: "cred-news.appspot.com",
+  messagingSenderId: "220168688339",
+  appId: "1:220168688339:web:fbdeaa4d704d3ed34f7a26",
+  measurementId: "G-HRLWYS50C8",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  var provider = new firebase.auth.GoogleAuthProvider();
-  
-  export { auth, provider };
-  export default db;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
