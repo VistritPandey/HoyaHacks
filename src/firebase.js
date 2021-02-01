7b8e0dc591c18599411bb729dcbcb8c7fdd79482
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-01 08:11:35 -0500

    Config settings removed

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2pi0RiZF3VeU2PIRx1VG9qu8J0J9TRjg",
-  authDomain: "cred-news.firebaseapp.com",
-  projectId: "cred-news",
-  storageBucket: "cred-news.appspot.com",
-  messagingSenderId: "220168688339",
-  appId: "1:220168688339:web:fbdeaa4d704d3ed34f7a26",
-  measurementId: "G-HRLWYS50C8",
+  // add your own cofig files
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
